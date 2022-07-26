6ff659056e585460bc7d27fa6230503bd7175d22
Matt Robinson <matt_rob@me.com>
2022-07-25 21:02:35 -0600

    Add argument parsing

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 
 from fitparse import FitFile
+import argparse
 import gzip
 import shutil
+from os.path import exists
+
+# - Assumes FIT file is gzip'd.
+# - Create empty database using 'sqlite3 file.db "VACUUM;"'.
 
 if __name__ == "__main__":
-    input = '/Users/mattrobinson/Downloads/6684555922.fit.gz'
-    output = '/Users/mattrobinson/Downloads/6684555922.fit'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fit_file', type=str, required=True)
+    parser.add_argument('--database', type=str, required=True)
+
+    args = parser.parse_args()
+    input_fit_file = args.fit_file
+    input_database = args.database
+
+    if not exists(input_fit_file) or not exists(input_database):
+        raise Exception("'input_fit_file' and 'database' must exist")
 
     # Unzip 'foo.fit.gz'
-    with gzip.open(input, 'rb') as f_in:
-        with open(output, 'wb') as f_out:
+    output_fit_file = input_fit_file + ".fit"
+    print("Converting '{}' => '{}'".format(input_fit_file, output_fit_file))
+    with gzip.open(input_fit_file, 'rb') as f_in:
+        with open(output_fit_file, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
     # Parse FIT file
-    for record in FitFile(output).get_messages('record'):
+    for record in FitFile(output_fit_file).get_messages('record'):
         def get_message_named(name):
             return ([record_data for record_data in record if record_data.name == name] or [None])[0]
 
