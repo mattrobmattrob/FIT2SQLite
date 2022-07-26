c81f567896bde52c2fed587675a8459eb798eafb
Matt Robinson <matt_rob@me.com>
2022-07-25 21:24:15 -0600

    Add database insertion/creation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
+from dis import dis
 from fitparse import FitFile
 import argparse
 import gzip
 import shutil
 from os.path import exists
+import sqlite3
 
 # - Assumes FIT file is gzip'd.
 # - Create empty database using 'sqlite3 file.db "VACUUM;"'.
@@ -27,6 +29,11 @@ if __name__ == "__main__":
         with open(output_fit_file, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
+    # Open database
+    db_connection = sqlite3.connect(input_database)
+    db_connection.execute('''CREATE TABLE IF NOT EXISTS activities (timestamp text, lat text, long text, heart_rate real, distance real)''')
+    db_cursor = db_connection.cursor()
+
     # Parse FIT file
     for record in FitFile(output_fit_file).get_messages('record'):
         def get_message_named(name):
@@ -36,12 +43,22 @@ if __name__ == "__main__":
         # for record_data in record:
         #     print(record_data.name)
 
+        timestamp = get_message_named("timestamp").value
         position_lat = get_message_named("position_lat").value
         position_long = get_message_named("position_long").value
-        timestamp = get_message_named("timestamp").value
         heart_rate = get_message_named("heart_rate").value
         distance = get_message_named("distance").value
 
         print(" * {}: latlng: ({}, {}), hr: {}, distance (m): {}".format(
             timestamp, position_lat, position_long, heart_rate, distance
         ))
+
+        # Run the SQL insertion
+        data_insert = """INSERT INTO activities
+                          (timestamp, lat, long, heart_rate, distance)
+                          VALUES (?, ?, ?, ?, ?);"""
+        data_tuple = (timestamp, position_lat, position_long, heart_rate, distance)
+        db_cursor.execute(data_insert, data_tuple)
+    
+    # Commit the INSERTs
+    db_connection.commit()
